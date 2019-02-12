cbb24fe5693b1f6ddf616beb8fce9c6e6d8517e1
yanalexander <yan.venera@univali.br>
2019-02-11 22:18:24 -0200

    correcao

---
+++ src/main/java/br/com/caelum/JpaConfigurator.java
@@ -0,0 +1,61 @@
+package br.com.caelum;
+
+import java.util.Properties;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+public class JpaConfigurator {
+
+	@Bean
+	public DataSource getDataSource() {
+	    DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+	    dataSource.setDriverClassName("org.postgresql.Driver");
+	    dataSource.setUrl("jdbc:postgresql://localhost:5432/projeto_jpa");
+	    dataSource.setUsername("postgres");
+	    dataSource.setPassword("postgres");
+
+	    return dataSource;
+	}
+
+	@Bean
+	public LocalContainerEntityManagerFactoryBean getEntityManagerFactory(DataSource dataSource) {
+		LocalContainerEntityManagerFactoryBean entityManagerFactory = new LocalContainerEntityManagerFactoryBean();
+
+		entityManagerFactory.setPackagesToScan("br.com.caelum");
+		entityManagerFactory.setDataSource(dataSource);
+
+		entityManagerFactory
+				.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+
+		Properties props = new Properties();
+
+		props.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQL9Dialect");
+		props.setProperty("hibernate.show_sql", "true");
+		props.setProperty("hibernate.hbm2ddl.auto", "create-drop");
+		props.setProperty("hibernate.format_sql", "true");
+
+		entityManagerFactory.setJpaProperties(props);
+		return entityManagerFactory;
+	}
+
+	@Bean
+	public JpaTransactionManager getTransactionManager(EntityManagerFactory emf) {
+		JpaTransactionManager transactionManager = new JpaTransactionManager();
+		transactionManager.setEntityManagerFactory(emf);
+
+		return transactionManager;
+	}
+
+}
